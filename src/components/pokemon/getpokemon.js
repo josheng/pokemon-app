a834f291688aa36760071c06cc85d6b2e1037e4a
Joshua Eng <engch28@gmail.com>
2022-04-29 14:03:48 +0800

    fetch kanto pkm

--- src/components/pokemon/getpokemon.js
+++ src/components/pokemon/getpokemon.js
@@ -3,21 +3,28 @@ import Button from "@mui/material/Button";
 import "./styles.css"
 
 const GetPokemon = () => {
-  const [pokemon, setPkm] = useState("");
+  const [pokemon, setPkm] = useState([]);
 
   const getKanto = () => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
       .then((response) => response.json())
       .then((data) => {
-        setPkm(data.name);
+        setPkm(data.results);
       });
   };
+
+  // for (let key in pokemon) {
+  //   console.log(`${key}: ${pokemon[key]}`);
+  // }
+  for (let pkm of pokemon) {
+    console.log(pkm.name);
+  }
 
   return (
     <div className="getpkm">
       <Button variant="contained" onClick={getKanto}>Choose Your Pokemon!</Button>
       <br></br>
-      <strong>{pokemon}</strong>
+      {/* <strong>{pokemon}</strong> */}
     </div>
   )
 };
